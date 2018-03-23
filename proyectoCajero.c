b4aa36fb95102e5ceb49bff1b37a4f596af94428
danivallejo <danivallejo@opendeusto.es>
2018-03-23 14:40:50 +0100

    He vuleto a anyadir el main que lo habia anyadido mal

---
+++ proyectoCajero.c
@@ -0,0 +1,42 @@
+#include "Utilidades.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	int a;
+
+	VerificarTarjeta();
+
+	do
+	{
+
+	printf("Este es el menu principal. Seleccione la operacion que desee: ");
+
+	scanf("%i", &a);
+
+	switch(a)	
+	{
+	case 1: 
+	SacarDinero(i);
+	break;
+	case 2:
+	MeterDinero();
+	break;
+	case 3:
+	ConsultarSaldo();
+	break;
+	case 4:
+	ConsultarMovimiento();
+	break;
+	case 5:
+	Transferencia();
+	case 6:
+	Salir();
+	break;
+	}
+	}while(a > 6 || a < 1);
+
+	return 0;
+}
